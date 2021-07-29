a70b9340bb94b6ddef2bb4d8de6fca219c83bf8c
melissaeg <melissa.escobar@utp.edu.co>
2021-07-29 12:09:07 -0500

    Add config for Bootstrap 5

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,9 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+import '../js/site.js'
+import 'scss/site.scss'
