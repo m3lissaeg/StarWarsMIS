89edfd9b54df1ad0db8e9aa21b8b5cbcc783abf1
tesla <melissa.escobar@utp.edu.co>
2021-08-23 18:47:33 -0500

    [WIP] Add crews_droids association

--- app/models/droid.rb
+++ app/models/droid.rb
@@ -1,4 +1,6 @@
 class Droid < ApplicationRecord
     validates :name,  presence: true, length: { maximum: 50 }
     validates :model,  presence: true, length: { maximum: 50 }
+
+    has_and_belongs_to_many :crews
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_23_221610) do
+ActiveRecord::Schema.define(version: 2021_08_23_230131) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "crews", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "crews_droids", force: :cascade do |t|
+    t.integer "crew_id"
+    t.integer "droid_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
