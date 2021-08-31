156f0834d215d485b7484d64f719c8d46e344daa
tesla <melissa.escobar@utp.edu.co>
2021-08-31 10:44:25 -0500

    Improve Crew tests: system and controller. Fixtures are still mad :c

--- test/controllers/crews_controller_test.rb
+++ test/controllers/crews_controller_test.rb
@@ -17,7 +17,11 @@ class CrewsControllerTest < ActionDispatch::IntegrationTest
 
   test "should create crew" do
     assert_difference('Crew.count') do
-      post crews_url, params: { crew: { name: @crew.name } }
+      post crews_url, params: { crew: { name: @crew.name , 
+                                      ship_ids: "1",
+                                      droid_ids: ["1"],
+                                      rebel_ids: ["1"]
+                                    } }
     end
 
     assert_redirected_to crew_url(Crew.last)
@@ -34,7 +38,11 @@ class CrewsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should update crew" do
-    patch crew_url(@crew), params: { crew: { name: @crew.name } }
+    patch crew_url(@crew), params: { crew: { name: @crew.name , 
+                                           ship_ids: "2",
+                                           droid_ids: ["2"],
+                                           rebel_ids: ["2"]
+                                         } }
     assert_redirected_to crew_url(@crew)
   end
 
--- test/system/crews_test.rb
+++ test/system/crews_test.rb
@@ -15,6 +15,9 @@ class CrewsTest < ApplicationSystemTestCase
     click_on "New Crew"
 
     fill_in "Name", with: @crew.name
+    page.select "X-wing fighter", from: "crew_ship_ids"
+    page.select "C-3PO", from: "crew_droid_ids"
+    page.select "Rey Skywalker", from: "crew_rebel_ids"
     click_on "Submit"
 
     assert_text "Crew was successfully created"
@@ -25,10 +28,22 @@ class CrewsTest < ApplicationSystemTestCase
     visit crews_url
     click_on "Edit", match: :first
 
-    fill_in "Name", with: @crew.name
+    fill_in "Name", with: "New cool name B)"
+    page.select "Millenium Falcon", from: "crew_ship_ids"
+    page.select "R2-D2", from: "crew_droid_ids"
+    page.select "Luke Skywalker", from: "crew_rebel_ids"
     click_on "Submit"
 
+    # Info filled and saved by the user should be equal to what is stored in DB
+    assert_equal "New cool name B)", Crew.order('id ASC').first.name
+    # assert_equal "R2-D2", Crew.order('id ASC').first.droids
+
     assert_text "Crew was successfully updated"
+    assert_text "New cool name B)"
+    assert_text "Millenium Falcon"
+    assert_text "R2-D2"
+    assert_text "Luke Skywalker"
+    
     click_on "Back"
   end
 
